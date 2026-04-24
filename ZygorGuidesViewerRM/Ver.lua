6165246030e8f84f50d4f196869acc1788ca5c0c
ErebusAres <erebusares@gmail.com>
2026-04-24 12:39:21 -0500

    3.0.140
    many NPC ID fixes and Localization Fixes.

--- ZygorGuidesViewerRM/Ver.lua
+++ ZygorGuidesViewerRM/Ver.lua
@@ -1,4 +1,4 @@
 assert(ZygorGuidesViewer,"Zygor Guides Viewer failed to load.")
-ZygorGuidesViewer.revision = 138
+ZygorGuidesViewer.revision = 140
 ZygorGuidesViewer.version = "3.0." .. ZygorGuidesViewer.revision
-ZygorGuidesViewer.date = "2026-04-23 20:09:35 -0600"
+ZygorGuidesViewer.date = "2026-04-24 12:40:35 -0600"
